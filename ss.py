f6ca75c5aadb97312933b8c45408757c0c2fe6ed
johnvon <handanlinzhang@163.com>
2020-01-30 12:50:28 +0800

    finish the algorithm implement

--- ss.py
+++ ss.py
@@ -1,4 +1,6 @@
 import copy
+from illustration import generate_uag
+
 
 def buildct(G, order,i,link):
 
@@ -13,9 +15,9 @@ def buildct(G, order,i,link):
     ct=[]
     z = order[0]
     order = order[1:]
-    S = get_neighbours(z)
-    chord_moral(G)
-    C = copy.deepcopy(S)
+    neighbours = get_neighbours(z, G)
+    chord_moral(G,neighbours)
+    C = copy.deepcopy(neighbours)
     C.add(z)
 
     if C.contain(G):
@@ -26,10 +28,33 @@ def buildct(G, order,i,link):
 
 
     for item in ct:
-        if item[1].contain(S):
+        if item[1].contain(neighbours):
             link.add((C,item))
 
     return ct
+
+
+def elim_node(G,z):
+    copy_G = copy.deepcopy(G)
+    for index, ele in enumerate(G):
+        if index == z:
+            copy_G.pop(z)
+        else:
+            copy_G[index].pop(z)
+
+
+def chord_moral(G,neighbours):
+
+    for node in neighbours:
+        for item in neighbours:
+            if node != item:
+                if G[node][item] == 0:
+                    G[node][item] = 1
+
+def get_neighbours(node_id, G):
+
+    neighbours=G[node_id]
+    return [idx for idx, i in enumerate(neighbours) if i == 1]
 
 def moralize(bn):
     """Construct the moral graph from bayesian network
@@ -41,7 +66,7 @@ def moralize(bn):
     parents=[]
 
     for item in bn:
-        parents.add([idx for idx, i in enumerate(item) if i==1])
+        parents.append([idx for idx, i in enumerate(item) if i==1])
     for item in parents:
         for s in item:
             for e in item:
@@ -50,3 +75,10 @@ def moralize(bn):
 
     return moral_graph
 
+if __name__ == '__main__':
+    t = [[0,1,1],[1,0,0],[1,0,0]]
+    chord_moral(t,[1,2])
+    print(t)
+    generate_uag(t)
+
+    #print(get_neighbours(0,t))
